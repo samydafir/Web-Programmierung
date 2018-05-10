e718ced9c19b22331199520f7eb1e6d7778b0694
unknown <samydafir@yahoo.de>
2018-05-10 10:13:36 +0200

    some more mako examples

--- 08/b08_a02/b08_a02.py
+++ 08/b08_a02/b08_a02.py
@@ -7,9 +7,12 @@ from mako.lookup import TemplateLookup
 templates = TemplateLookup(directories=['templates/'], module_directory='modules/', 
   output_encoding='utf-8', encoding_errors='replace')
 
-curr_template = templates.get_template("output.html")
+curr_template = templates.get_template("header.py")
 
-print "Content-type: text/html\n" 
-print
+curr_template.render() 
+
+curr_template = templates.get_template("index.html")
 
 print(curr_template.render(teststring="test", style="btn btn-success btn-block"))
+
+
---
+++ 08/b08_a02/templates/header.py
@@ -0,0 +1,4 @@
+<%
+    print "Content-Type: text/html\n"
+    print
+%>
